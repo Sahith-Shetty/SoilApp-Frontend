bd67c4f0ac8fff1ac680b0e141000e2da6498ea4
Sahith-Shetty <60418289+Sahith-Shetty@users.noreply.github.com>
2020-08-23 16:53:10 +0530

    Update soilsearch.component.ts

--- src/app/soilsearch/soilsearch.component.ts
+++ src/app/soilsearch/soilsearch.component.ts
@@ -9,18 +9,39 @@ import { SoildataService } from '../soildata.service';
 })
 export class SoilsearchComponent implements OnInit {
   searchControl: FormControl = new FormControl();
+  nControl: FormControl = new FormControl();
+  pControl: FormControl = new FormControl();
+  kControl: FormControl = new FormControl();
   allSoilDetails = [];
+  searchObj: any = {};
   constructor(private soilService: SoildataService) { }
 
   ngOnInit(): void {
-  this.searchControl.valueChanges.subscribe(value => {
-	console.log(value);
- this.soilService.searchSoilDetails(value).subscribe(data => {
+  this.searchControl.valueChanges.subscribe(value => this.updateValues(value));
+  this.nControl.valueChanges.subscribe(value => {this.searchObj.n = value; this.updateNPKValues();  });
+  this.pControl.valueChanges.subscribe(value => {this.searchObj.p = value; this.updateNPKValues();  });
+  this.kControl.valueChanges.subscribe(value => {this.searchObj.k = value; this.updateNPKValues();  });
+  }
+
+  updateValues(value) {
+    this.nControl.setValue('', {emitEvent: false});
+    this.pControl.setValue('', {emitEvent: false});
+    this.kControl.setValue('', {emitEvent: false});
+    this.soilService.searchSoilDetails(value).subscribe(data => {
+          console.log(data);
+          this.allSoilDetails = data;
+        });
+  }
+
+  updateNPKValues() {
+    const value = this.searchObj;
+    this.searchControl.setValue('', {emitEvent: false});
+    Object.keys(this.searchObj).map((e) => {if (this.searchObj[e] == '') { delete this.searchObj[e]; }});
+    console.log(value);
+    this.soilService.searchByNPK(value).subscribe(data => {
       console.log(data);
       this.allSoilDetails = data;
     });
-  });
   }
-
 
 }
